59e7812af9743cbc07cfe7ed979b3a6c4f47bca4
劲松 孙 <624536826@qq.com>
2023-08-01 23:25:54 +0800

    test

---
+++ script/win7.bat
@@ -0,0 +1,24 @@
+@echo off
+chcp 65001
+
+echo 获取当前的执行策略
+for /f "usebackq" %%i in (`powershell Get-ExecutionPolicy`) do set currentPolicy=%%i
+echo.
+
+echo 当前策略为 %currentPolicy%
+echo.
+
+echo 设置策略为RemoteSigned
+powershell Set-ExecutionPolicy RemoteSigned
+echo.
+
+echo 执行powershell脚本
+echo.
+powershell -File "%~dp0win7.ps1"
+echo.
+
+echo 还原策略为 %currentPolicy%
+echo.
+powershell Set-ExecutionPolicy %currentPolicy%
+
+pause
---
+++ script/win7.ps1
@@ -0,0 +1,10 @@
+get-executionpolicy
+$server = (Get-VMNetworkAdapter -VMName "Windows 7 x64" | Select-Object -ExpandProperty IPAddresses)[0]
+
+$username = "johnson"
+$password = "33872006"
+
+cmdkey /generic:TERMSRV/$server /user:$username /pass:$password
+mstsc /v:$server /admin /f
+
+cmdkey /delete:TERMSRV/$server
