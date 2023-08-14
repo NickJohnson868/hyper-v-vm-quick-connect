d7a41518e1409c621dfc16e06696a39a9c678cfe
孙劲松 <jinsongsun868@gmail.com>
2023-08-14 15:58:30 +0800

    1

--- script/win7.ps1
+++ script/win7.ps1
@@ -1,35 +1,27 @@
 # 定义虚拟机名称数组
-$vmNames = @("Windows 10 x64 LTSC", "Windows 7 x64")
+$vmNames = Get-VM | Select-Object -ExpandProperty Name
 
-while ($true)
-{
-    # 显示选择菜单
+# 定义显示选择菜单的函数
+function Show-Menu {
     Write-Host "请选择要连接的虚拟机："
+    $vmStatuses = Get-VM | Select-Object -ExpandProperty State
     for ($i = 0; $i -lt $vmNames.Length; $i++)
     {
-        Write-Host ("[" + ($i + 1) + "]`t" + $vmNames[$i])
+        Write-Host ("[" + ($i + 1) + "]`t" + $vmNames[$i] + "(" + $vmStatuses[$i] + ")")
     }
     Write-Host "[x]`t关闭所有虚拟机"
     Write-Host "[q]`t退出"
+}
 
-    # 获取用户输入
-    $choice = Read-Host "请输入您的选择"
-    if ($choice -eq "q")
-    {
-        exit
-    }
-    elseif ($choice -eq "x")
-    {
-        Write-Host "正在关闭所有虚拟机..."
-        Get-VM | where {$_.State -eq 'Running'} | Stop-VM
-        Write-Host "关闭完成`n"
-        continue
-    }
+# 定义关闭所有虚拟机的函数
+function Stop-AllVMs {
+    Write-Host "正在关闭所有虚拟机..."
+    Get-VM | where {$_.State -eq 'Running'} | Stop-VM
+    Write-Host "关闭完成`n"
+}
 
-    # 获取虚拟机名称
-    $vmName = $vmNames[$choice - 1]
-
-    Write-Host ""
+# 定义启动虚拟机的函数
+function Start-VMByName($vmName) {
     # 检查虚拟机状态
     $vm = Get-VM -VMName $vmName
     if ($vm.State -ne "Running")
@@ -48,7 +40,10 @@ while ($true)
 
         Write-Host "虚拟机已启动完毕"
     }
+}
 
+# 定义获取虚拟机 IP 地址的函数
+function Get-VMIPAddress($vmName) {
     # 获取虚拟机 IP 地址
     Write-Host "尝试获取虚拟机IP..."
     $startTime = Get-Date
@@ -61,23 +56,56 @@ while ($true)
             if (((Get-Date) - $startTime).TotalSeconds -gt 10)
             {
                 Write-Host "获取IP超时"
-                continue
+                return $null
             }
         }
         Write-Host "..."
     } while ($ipAddresses.Length -eq 0)
 
-    if ($ipAddresses.Length -gt 0)
+    return $ipAddresses[0]
+}
+
+# 定义连接到虚拟机的函数
+function Connect-ToVM($server, $username, $password) {
+    cmdkey /generic:TERMSRV/$server /user:"$username" /pass:"$password"
+    mstsc /v:$server /f
+
+    cmdkey /delete:TERMSRV/$server
+}
+
+while ($true)
+{
+    # 显示选择菜单并获取用户输入
+    Show-Menu
+    $choice = Read-Host "请输入您的选择"
+
+    # 处理用户输入
+    if ($choice -eq "q")
+    {
+        exit
+    }
+    elseif ($choice -eq "x")
+    {
+        Stop-AllVMs
+        continue
+    }
+
+    if ($choice -lt 0 -or $choice -gt $vmNames.Length)
+    {
+        Write-Host "输入不合法`n"
+        continue
+    }
+
+    # 获取虚拟机名称并启动虚拟机
+    $vmName = $vmNames[$choice - 1]
+    Start-VMByName $vmName
+
+    # 获取虚拟机 IP 地址并连接到虚拟机
+    $server = Get-VMIPAddress $vmName
+
+    if ($server)
     {
         Write-Host "正在连接虚拟机..."
-        $server = $ipAddresses[0]
-
-        $username = "62453"
-        $password = "1"
-
-        cmdkey /generic:TERMSRV/$server /user:"$username" /pass:"$password"
-        mstsc /v:$server /f
-
-        cmdkey /delete:TERMSRV/$server
+        Connect-ToVM $server "62453" "1"
     }
 }
